0592ff31904efd466ab95cc2240dcdf1a598c9bd
KG <davidgjy@163.com>
2017-04-12 22:48:29 +0800

    Elk demo

---
+++ elk-app/src/main/java/org/genesis/arena/elk/ElkLog4jTest2.java
@@ -0,0 +1,13 @@
+package org.genesis.arena.elk;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by KG on 17/3/27.
+ */
+public class ElkLog4jTest2 {
+    private static final Logger logger = Logger.getLogger(ElkLog4jTest2.class);
+    public static void main(String[] args) throws Exception {
+        logger.debug("这是应用B的日志!");
+    }
+}
